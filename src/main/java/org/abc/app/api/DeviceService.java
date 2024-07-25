51d571d83a38dbe10045e95448c4bd7d3da81899
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-25 21:29:08 +0400

    feat: add device service

---
+++ src/main/java/org/abc/app/api/DeviceService.java
@@ -0,0 +1,18 @@
+package org.abc.app.api;
+
+import org.abc.app.device.Device;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DeviceService {
+
+    public List<Device> getAll() {
+
+        //TODO: implement method body
+
+        return new ArrayList<>();
+    }
+}
