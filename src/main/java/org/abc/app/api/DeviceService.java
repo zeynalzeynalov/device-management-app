2773e7df215ad0084311844a0e48eb9d3b0c410a
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-25 21:48:59 +0400

    feat: add device entity repository

--- src/main/java/org/abc/app/api/DeviceService.java
+++ src/main/java/org/abc/app/api/DeviceService.java
@@ -1,6 +1,7 @@
 package org.abc.app.api;
 
 import org.abc.app.device.Device;
+import org.abc.app.device.DeviceRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,6 +9,12 @@ import java.util.List;
 
 @Service
 public class DeviceService {
+
+    private final DeviceRepository deviceRepository;
+
+    public DeviceService(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
 
     public List<Device> getAll() {
 
---
+++ src/main/java/org/abc/app/device/DeviceRepository.java
@@ -0,0 +1,6 @@
+package org.abc.app.device;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+}
