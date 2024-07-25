4c7478b18b2d7dbd4f458f1e65011a559e96e66d
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-25 21:34:09 +0400

    feat: implement rest api controller for devices

---
+++ src/main/java/org/abc/app/api/DeviceController.java
@@ -0,0 +1,22 @@
+package org.abc.app.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/device")
+@RestController
+public class DeviceController {
+
+    private final DeviceService deviceService;
+
+    public DeviceController(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<String> checkStatus() {
+        return ResponseEntity.ok("API is running.");
+    }
+}
