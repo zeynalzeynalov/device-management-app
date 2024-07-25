60ce6d361d3e97856c40da8343de46a984c2ee2d
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-25 21:14:16 +0400

    feat: add spring boot dependency

---
+++ src/main/java/org/abc/app/DeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package org.abc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagementApplication.class, args);
+    }
+
+}
