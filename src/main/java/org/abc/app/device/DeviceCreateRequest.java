a49b9190d62cf2864ab7f84178c94058e144e0d0
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-26 19:15:12 +0400

    feat: add non blank constraint to create device request fields

--- src/main/java/org/abc/app/device/DeviceCreateRequest.java
+++ src/main/java/org/abc/app/device/DeviceCreateRequest.java
@@ -1,5 +1,6 @@
 package org.abc.app.device;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -8,10 +9,12 @@ import org.hibernate.validator.constraints.Length;
 public class DeviceCreateRequest {
 
     @NotNull(message = "Device name can not be null")
+    @NotBlank(message = "Device name can not be blank")
     @Length(min = 3, max = 50, message = "Device name must have size of between 3 and 50 characters")
     public String name;
 
     @NotNull(message = "Device brand can not be null")
+    @NotBlank(message = "Device brand can not be blank")
     @Length(min = 3, max = 50, message = "Device brand must have size of between 3 and 50 characters")
     public String brand;
 }
