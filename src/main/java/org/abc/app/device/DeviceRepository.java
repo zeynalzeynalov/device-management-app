04553d99b7135ce801c5f1a1f87114b895d3485c
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-26 18:15:32 +0400

    chore: new custom exception for device

--- src/main/java/org/abc/app/device/DeviceRepository.java
+++ src/main/java/org/abc/app/device/DeviceRepository.java
@@ -7,4 +7,27 @@ import java.util.List;
 public interface DeviceRepository extends JpaRepository<Device, Long> {
 
     List<Device> findByBrandContaining(String brand);
+
+    class DeviceNotFoundException extends RuntimeException {
+
+        public DeviceNotFoundException(long deviceId) {
+            super(String.format("Device with id=%d not found", deviceId));
+        }
+
+        public DeviceNotFoundException(String message) {
+            super(message);
+        }
+
+        public DeviceNotFoundException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public DeviceNotFoundException(Throwable cause) {
+            super(cause);
+        }
+
+        public DeviceNotFoundException() {
+            super("Device not found");
+        }
+    }
 }
