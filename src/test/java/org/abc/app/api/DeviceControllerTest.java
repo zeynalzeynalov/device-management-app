78ce4130f18d0ea6447dc79890a0b18ee39ab6e0
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-27 19:19:36 +0400

    test: add more unit tests for device controller

--- src/test/java/org/abc/app/api/DeviceControllerTest.java
+++ src/test/java/org/abc/app/api/DeviceControllerTest.java
@@ -1,9 +1,11 @@
 package org.abc.app.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.abc.app.common.RestResponse;
 import org.abc.app.device.Device;
 import org.abc.app.device.DeviceCreateRequest;
 import org.abc.app.device.DeviceRepository;
+import org.abc.app.device.DeviceUpdateRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +40,7 @@ public class DeviceControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1/status"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data").value("API is running."));
     }
 
@@ -50,7 +52,7 @@ public class DeviceControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data[0].id").value(device.getId()))
                 .andExpect(jsonPath("data[0].name").value(device.getName()))
                 .andExpect(jsonPath("data[0].brand").value(device.getBrand()));
@@ -63,7 +65,7 @@ public class DeviceControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data").isEmpty());
     }
 
@@ -75,21 +77,21 @@ public class DeviceControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data.id").value(device.getId()))
                 .andExpect(jsonPath("data.name").value(device.getName()))
                 .andExpect(jsonPath("data.brand").value(device.getBrand()));
     }
 
     @Test
-    public void getDeviceById_whenNotExists_shouldReturnErrorMessage() throws Exception {
-        when(deviceService.getById(1)).thenThrow(new DeviceRepository.DeviceNotFoundException(1));
+    public void getDeviceById_whenNotExists_shouldReturnFailure() throws Exception {
+        when(deviceService.getById(666)).thenThrow(new DeviceRepository.DeviceNotFoundException(666));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1/1"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1/666"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("fail"))
-                .andExpect(jsonPath("data").value("Device with id=1 not found"));
+                .andExpect(jsonPath("status").value(RestResponse.FAIL))
+                .andExpect(jsonPath("data").value("Device with id=666 not found"));
     }
 
     @Test
@@ -100,7 +102,7 @@ public class DeviceControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1/filter?brand=test"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data[0].id").value(device.getId()))
                 .andExpect(jsonPath("data[0].name").value(device.getName()))
                 .andExpect(jsonPath("data[0].brand").value(device.getBrand()));
@@ -114,17 +116,17 @@ public class DeviceControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/devices/v1/filter?brand=test_new"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data").isEmpty());
     }
 
     @Test
-    public void createDevice_whenSuccessful_shouldReturnSuccess() throws Exception {
+    public void createDevice_whenSuccessful_shouldReturnSuccessAndDevice() throws Exception {
         DeviceCreateRequest createRequest = new DeviceCreateRequest();
-        createRequest.setName("Created device name");
-        createRequest.setBrand("Created device brand");
+        createRequest.setName("     Created device name      ");
+        createRequest.setBrand("      Created device brand         ");
 
-        Device device = Device.builder().id(1L).name(createRequest.name).brand(createRequest.brand).build();
+        Device device = Device.builder().id(1L).name(createRequest.name.trim()).brand(createRequest.brand.trim()).build();
 
         when(deviceService.create(createRequest)).thenReturn(device);
 
@@ -133,10 +135,94 @@ public class DeviceControllerTest {
                         .content(objectMapper.writeValueAsString(createRequest)))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("status").value("success"))
+                .andExpect(jsonPath("status").value(RestResponse.SUCCESS))
                 .andExpect(jsonPath("data.id").value(device.getId()))
-                .andExpect(jsonPath("data.name").value(createRequest.getName()))
-                .andExpect(jsonPath("data.brand").value(createRequest.getBrand()));
+                .andExpect(jsonPath("data.name").value(createRequest.getName().trim()))
+                .andExpect(jsonPath("data.brand").value(createRequest.getBrand().trim()));
     }
 
+    @Test
+    public void createDevice_whenMissingField_shouldReturnFailure() throws Exception {
+        DeviceCreateRequest createRequest = new DeviceCreateRequest();
+        createRequest.setName("Created device name");
+
+        Device device = Device.builder().id(1L).name(createRequest.name).brand(createRequest.brand).build();
+
+        when(deviceService.create(createRequest)).thenReturn(device);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/devices/v1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("status").value(RestResponse.FAIL))
+                .andExpect(jsonPath("data").value("Device brand can not be blank"));
+    }
+
+    @Test
+    public void updateDevice_whenSuccessful_shouldReturnSuccessAndDevice() throws Exception {
+        DeviceUpdateRequest updateRequest = new DeviceUpdateRequest();
+        updateRequest.setName("       Updated device name      ");
+        updateRequest.setBrand("       Updated device brand       ");
+
+        Device device = Device.builder()
+                .id(1L)
+                .name(updateRequest.getName().trim())
+                .brand(updateRequest.getBrand().trim())
+                .build();
+
+        when(deviceService.update(1L, updateRequest)).thenReturn(device);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/devices/v1/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value(RestResponse.SUCCESS))
+                .andExpect(jsonPath("$.data.id").value(device.getId()))
+                .andExpect(jsonPath("$.data.name").value(updateRequest.getName().trim()))
+                .andExpect(jsonPath("$.data.brand").value(updateRequest.getBrand().trim()));
+    }
+
+    @Test
+    public void updateDevice_whenPartially_shouldReturnSuccessAndDevice() throws Exception {
+        DeviceUpdateRequest updateRequest = new DeviceUpdateRequest();
+        updateRequest.setName("Updated device name");
+        updateRequest.setBrand(null);
+
+        Device device = Device.builder()
+                .id(1L)
+                .name(updateRequest.getName())
+                .brand(updateRequest.getBrand())
+                .build();
+
+        when(deviceService.update(1L, updateRequest)).thenReturn(device);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/devices/v1/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value(RestResponse.SUCCESS))
+                .andExpect(jsonPath("$.data.id").value(device.getId()))
+                .andExpect(jsonPath("$.data.name").value(device.getName()))
+                .andExpect(jsonPath("$.data.brand").value(device.getBrand()));
+    }
+
+    @Test
+    public void updateDevice_whenDeviceNotExists_shouldReturnFailure() throws Exception {
+        DeviceUpdateRequest updateRequest = new DeviceUpdateRequest();
+        updateRequest.setName("Updated device name");
+        updateRequest.setBrand(null);
+
+        when(deviceService.update(666, updateRequest)).thenThrow(new DeviceRepository.DeviceNotFoundException(666));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/devices/v1/666")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value(RestResponse.FAIL))
+                .andExpect(jsonPath("data").value("Device with id=666 not found"));
+    }
 }
